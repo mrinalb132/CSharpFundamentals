d7a8fc3f7be5910f34247290cf50aa5bdb6b72d0
Mrinal Banerjee <bannymbps@gmail.com>
2024-03-15 08:53:21 +0530

    Add project files.

---
+++ ErrorHandling/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    public static class Calculator
+    {
+        public static int Calculate(int n1, int n2, string operation)
+        {
+            switch(operation)
+            {
+                case "+": return n1 + n2;
+                case "-": return n1 - n2;
+                case "*": return n1 * n2;
+                case "/": return n1 / n2;
+                default:
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(operation), "The operation is not supported");
+                    }
+            }
+        }
+    }
+}
---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter the first no:");
+            int num1 = int.Parse(ReadLine()!);
+            WriteLine("Enter the second no:");
+            int num2 = int.Parse(ReadLine()!);
+            WriteLine("Enter the operation:"); 
+            string ops = ReadLine()!.ToUpperInvariant();
+            try
+            {
+                var result = Calculator.Calculate(num1, num2, ops);
+                Write($"The result is : {result}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Exception has raised: {ex}");
+            }
+
+            finally
+            {
+                WriteLine("Execution completed");
+            }
+           
+
+            
+            
+        }
+    }
+}
